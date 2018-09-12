fbbf91e2334fd4b94d4a45db3a5d5ab20ec595fd
a503-26 <a503-26@localhost>
2018-09-12 14:55:00 +0900

    1

--- 0912springstart/src/main/java/Main.java
+++ 0912springstart/src/main/java/Main.java
@@ -9,15 +9,15 @@ public class Main {
 	public static void main(String[] args) {
 		//GoodDao 인스턴스 생성
 		GoodDao dao = GoodDaoFactory.create();
-		//System.out.println(dao.hashCode());
+		System.out.println(dao.hashCode());
 		//필요한 메소드 호출
 		List<GoodVO> list = dao.getGood();
 		//메소드 호출 결과 출력
 		for(GoodVO vo : list) {
 			System.out.println(vo);
 		}
-		//dao = GoodDaoFactory.create();
-		//System.out.println(dao.hashCode());
+		dao = GoodDaoFactory.create();
+		System.out.println(dao.hashCode());
 		
 	}
 
