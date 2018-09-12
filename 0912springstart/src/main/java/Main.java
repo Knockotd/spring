7c59f9da82ba9b7477787e141bab8db8b6c86555
a503-26 <a503-26@localhost>
2018-09-12 15:12:53 +0900

    180912 Spring Start

--- 0912springstart/src/main/java/Main.java
+++ 0912springstart/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.List;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import dao.GoodDao;
 import dao.GoodDaoFactory;
 import vo.GoodVO;
@@ -7,18 +9,22 @@ import vo.GoodVO;
 public class Main {
 
 	public static void main(String[] args) {
-		//GoodDao 인스턴스 생성
-		GoodDao dao = GoodDaoFactory.create();
+		// GoodDao 인스턴스 생성
+		// GoodDao dao = new GoodDao();
+		// GoodDao dao = GoodDaoFactory.create();
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(GoodDaoFactory.class);
+		GoodDao dao = context.getBean("create", GoodDao.class);
 		System.out.println(dao.hashCode());
-		//필요한 메소드 호출
+		// 필요한 메소드 호출
 		List<GoodVO> list = dao.getGood();
-		//메소드 호출 결과 출력
-		for(GoodVO vo : list) {
+		// 메소드 호출 결과 출력
+		for (GoodVO vo : list) {
 			System.out.println(vo);
 		}
-		dao = GoodDaoFactory.create();
+		// dao = GoodDaoFactory.create();
+		dao = context.getBean("create", GoodDao.class);
 		System.out.println(dao.hashCode());
-		
+
 	}
 
 }
